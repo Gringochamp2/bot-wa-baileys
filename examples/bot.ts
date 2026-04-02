88a9ec7b9b3471616efe6ca24bac1ad1ba96445f
Gringochamp2 <gringochampion2@gmail.com>
2026-04-02 09:08:53 -0400

    Update bot.ts

--- examples/bot.ts
+++ examples/bot.ts
@@ -1,4 +1,4 @@
-import { BaileysClass } from '../src/baileys.ts'; // Mudamos de /lib/ para /src/
+import { BaileysClass } from '../src/baileys.ts';
 
 const botBaileys = new BaileysClass();
 
@@ -6,17 +6,16 @@ botBaileys.on('qr', (qr) => console.log("NOVO QR CODE: ", qr));
 botBaileys.on('ready', () => console.log('🚀 MÁQUINA LIGADA: MONITORANDO FLASH DELIVERY JIPA 2'));
 
 botBaileys.on('message', async (message) => {
-    // FILTRO 1: Nome exato do grupo (Segurança total)
     const grupoAlvo = "FLASH DELIVERY JIPA 2";
 
     if (message.groupName === grupoAlvo) {
         
-        // FILTRO 2: É figurinha? (Sticker)
+        // FILTRO 2: É figurinha?
         // FILTRO 3: O número está nos seus contatos salvos?
         if (message.type === 'sticker' && message.isContact) {
             
             // FILTRO 4: Resposta com REPRODUÇÃO (Destaque/Quoted)
-            // Isso garante que o ADM veja que você marcou o pedido certo
+            // Corrigido: Removido o '#' que estava após 'message'
             await botBaileys.sendText(message.from, "Eu", { quoted: message });
 
             console.log(`✅ Pedido pego de: ${message.pushName}`);
